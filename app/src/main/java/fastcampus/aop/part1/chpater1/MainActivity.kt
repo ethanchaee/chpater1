69cecccbabc78c3e470c182e607d7065a4377494
ethanchae <boschae36@gmail.com>
2022-10-10 13:30:17 +0900

    feat : 기능 구현

--- app/src/main/java/fastcampus/aop/part1/chpater1/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part1/chpater1/MainActivity.kt
@@ -1,11 +1,31 @@
 package fastcampus.aop.part1.chpater1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var number: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numberTextView = findViewById<TextView>(R.id.number_text_view)
+        numberTextView.text = number.toString()
+
+        val countUpButton = findViewById<Button>(R.id.button)
+        countUpButton.setOnClickListener {
+            number += 1
+            numberTextView.text = number.toString()
+        }
+
+        val resetButton = findViewById<Button>(R.id.button2)
+        resetButton.setOnClickListener {
+            number = 0
+            numberTextView.text = number.toString()
+        }
     }
 }
